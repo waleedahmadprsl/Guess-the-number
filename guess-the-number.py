4719d1c9006b45c4d80c40f2581d012a713f0488
Waleed Ahmad <137624391+waleedahmadprsl@users.noreply.github.com>
2023-06-25 15:12:11 +0500

    Update guess-the-number.py

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,3 +1,6 @@
+#This game is called "Guess the Number." The player has to guess a randomly generated number within a certain range.
+
+
 import random
 
 def guess_the_number():
@@ -19,3 +22,22 @@ def guess_the_number():
             break
 
 guess_the_number()
+
+
+
+#instructions 
+#To run the game in VS Code, follow these steps:
+
+# Open VS Code and create a new Python file. For example, name it guess_the_number.py.
+
+# Copy the code into the file.
+
+# Save the file.
+
+# Open the integrated terminal in VS Code by going to View -> Terminal or using the shortcut Ctrl+ backtick.
+
+# In the terminal, navigate to the directory where you saved the Python file using the cd command. For example, if the file is in the Desktop directory, you can use the command cd Desktop.
+
+# Run the Python file with the command python guess_the_number.py.
+
+# The game will start, and you can play by guessing numbers within the given range. The program will provide feedback if your guess is too high or too low. Once you guess the correct number, it will display the number of attempts you took.
